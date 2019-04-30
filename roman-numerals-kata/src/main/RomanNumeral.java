3cb53af06133c98e7f198d550b27651f131293c2
Mathieu Leblanc <0mat.leb2@gmail.com>
2019-04-30 12:45:22 -0400

    Roman numerals kata - Parse numeral in reverse order and sum additive and subtractive letters

--- roman-numerals-kata/src/main/RomanNumeral.java
+++ roman-numerals-kata/src/main/RomanNumeral.java
@@ -3,20 +3,21 @@ package main;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Optional;
 
 public class RomanNumeral {
 	
-	private static final Map<String, Integer> LETTER_VALUES = initLetterValues();
+	private static final Map<Character, Integer> LETTER_VALUES = initLetterValues();
 	
-	private static Map<String, Integer> initLetterValues() {
-		Map<String, Integer> letterValues = new HashMap<>();
-		letterValues.put("I", 1);
-		letterValues.put("V", 5);
-		letterValues.put("X", 10);
-		letterValues.put("L", 50);
-		letterValues.put("C", 100);
-		letterValues.put("D", 500);
-		letterValues.put("M", 1000);
+	private static Map<Character, Integer> initLetterValues() {
+		Map<Character, Integer> letterValues = new HashMap<>();
+		letterValues.put('I', 1);
+		letterValues.put('V', 5);
+		letterValues.put('X', 10);
+		letterValues.put('L', 50);
+		letterValues.put('C', 100);
+		letterValues.put('D', 500);
+		letterValues.put('M', 1000);
 		return Collections.unmodifiableMap(letterValues);
 	}
 	
@@ -27,11 +28,27 @@ public class RomanNumeral {
 	}
 
 	public int getInt() {
-		int value = 0;
-		for (String letter : numeral.split("")) {
-			value += LETTER_VALUES.get(letter);
+		int totalValue = 0;
+		Optional<Character> lastLetter = Optional.empty();
+		
+		for (int i = numeral.length(); i-- > 0;) {
+			char letter = numeral.charAt(i);
+			totalValue += getLetterValue(letter, lastLetter);
+			lastLetter = Optional.of(letter);			
 		}
-		return value;
+		
+		return totalValue;
+	}
+	
+	private int getLetterValue(char letter, Optional<Character> lastLetter) {
+		
+		int letterValue = LETTER_VALUES.get(letter);
+		
+		if ( !lastLetter.isPresent() || letterValue >= LETTER_VALUES.get(lastLetter.get()) ) {
+			return letterValue;
+		}
+		
+		return -letterValue;	
 	}
 
 }
