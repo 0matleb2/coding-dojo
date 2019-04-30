d98ce321cae5b79918720d184967fbd05eea1e22
Mathieu Leblanc <0mat.leb2@gmail.com>
2019-04-30 08:58:03 -0400

    Roman numerals kata - Write tests for roman numeral letters X, L, C, D and M

--- roman-numerals-kata/src/test/RomanNumeralTest.java
+++ roman-numerals-kata/src/test/RomanNumeralTest.java
@@ -12,7 +12,7 @@ class RomanNumeralTest {
 	RomanNumeral romanNumeral;
 
 	@BeforeEach
-	void setUp() throws Exception {
+	void setUp() {
 		romanNumeral = new RomanNumeral();
 	}
 
@@ -32,6 +32,51 @@ class RomanNumeralTest {
 		int result = romanNumeral.getInt();
 		
 		assertEquals(5, result);
+	}
+	
+	@Test
+	void givenX_whenGetInt_thenReturn10() {
+		romanNumeral.setNumeral("X");
+		
+		int result = romanNumeral.getInt();
+		
+		assertEquals(10, result);
+	}
+	
+	@Test
+	void givenL_whenGetInt_thenReturn50() {
+		romanNumeral.setNumeral("L");
+		
+		int result = romanNumeral.getInt();
+		
+		assertEquals(50, result);
+	}
+	
+	@Test
+	void givenC_whenGetInt_thenReturn100() {
+		romanNumeral.setNumeral("C");
+		
+		int result = romanNumeral.getInt();
+		
+		assertEquals(100, result);
+	}
+	
+	@Test
+	void givenD_whenGetInt_thenReturn500() {
+		romanNumeral.setNumeral("D");
+		
+		int result = romanNumeral.getInt();
+		
+		assertEquals(500, result);
+	}
+	
+	@Test
+	void givenM_whenGetInt_thenReturn1000() {
+		romanNumeral.setNumeral("M");
+		
+		int result = romanNumeral.getInt();
+		
+		assertEquals(1000, result);
 	}
 
 }
